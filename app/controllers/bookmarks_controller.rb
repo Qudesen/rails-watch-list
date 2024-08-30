2ffaa63e0199da61c4a1b5220ce9fe1d45e19c8d
Quentin Desgrippes <desgrippes.quentin@gmail.com>
2024-08-30 17:48:17 +0200

    maj

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -8,11 +8,17 @@ class BookmarksController < ApplicationController
     @list = List.find(params[:list_id])
     @bookmark = Bookmark.new(bookmark_params)
     @bookmark.list = @list
-    if @bookmark.save!
+    if @bookmark.save
       redirect_to list_path(@list)
     else
       render :new, status: :unprocessable_entity
     end
+  end
+
+  def destroy
+    @bookmark = Bookmark.find(params[:list_id])
+    @bookmark.destroy
+    redirect_to list_path(@bookmark), status: :see_other
   end
 
   private
